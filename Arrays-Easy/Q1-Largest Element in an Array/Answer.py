1ef178533955403cd1ff1b2be2f10d4695224346
Mitesh Rege <miteshrege4@gmail.com>
2025-11-19 18:16:19 +0530

    Feat(Q1): Added solution and problem statement for finding the largest element in an array

---
+++ Arrays-Easy/Q1-Largest Element in an Array/Answer.py
@@ -0,0 +1,28 @@
+# Solution1: Sorting
+# Complexity Analysis:
+# Time Complexity: O(n log n) due to the sorting operation.
+# Code:
+
+from typing import List
+def sortArr(arr: List[int]) -> int:
+    arr.sort()
+    return arr[-1]
+
+
+# Solution 2: Linear Scan
+# Complexity Analysis:
+# Time Complexity: O(n) as we traverse the array only once.
+# Code:
+
+from typing import List
+def largestElement(arr: List[int]) -> int:
+    max_element = arr[0]
+    for num in arr:
+        if num > max_element:
+            max_element = num
+    return max_element
+
+if __name__ == "__main__":
+    arr = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+    print("Largest element using sorting:", sortArr(arr))
+    print("Largest element using linear scan:", largestElement(arr))
